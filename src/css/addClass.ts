f43c925ab28caff0335022987d8d4f0d540fbd8d
DriedSponge <jortuck0810@gmail.com>
2020-09-24 18:10:50 -0700

    Made it so addClass can accept arrays and it can chain.

--- src/css/addClass.ts
+++ src/css/addClass.ts
@@ -1,7 +1,16 @@
 import {col} from "../main/main";
 
-col.prototype.addClass = (className) => {
-    col.prototype.this.forEach((element) => {
-        element.classList.add(className);
-    })
+col.prototype.addClass = (...args) => {
+    if(typeof args[0] === 'string'){
+        col.prototype.this.forEach((element) => {
+            element.classList.add(args[0]);
+        })
+    }else if(args[0] instanceof Array){
+        args[0].forEach((cls) => {
+            col.prototype.this.forEach((element) => {
+                element.classList.add(cls);
+            })
+        })
+    }
+    return col.prototype;
 }
