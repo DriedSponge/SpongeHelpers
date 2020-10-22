27c5e0e757fea9305b0b87108ca533c53eb76d74
DriedSponge <jordan@driedsponge.net>
2020-10-21 17:38:18 -0700

    Html function now returns an items inner html when no html to set is present

--- src/html/html.ts
+++ src/html/html.ts
@@ -1,13 +1,18 @@
 import {SpongeHelpers} from "../main/main"
 declare module "../main/main"{
     export interface SpongeHelpers{
-        html(html:string): SpongeHelpers;
+        html(html?:string): SpongeHelpers;
     }
 }
-const html = function(html:string){
-    this.selected.forEach((element) => {
-        element.innerHTML = html;
-    })
+const html = function(html?:string){
+    if(html == null){
+        return this.selected[0].innerHTML;
+    }else{
+        this.selected.forEach((element) => {
+            element.innerHTML = html;
+        })
+    }
+
     return this;
 }
 SpongeHelpers.prototype.html = html;
